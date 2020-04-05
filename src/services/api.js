475ce8599d2d178f78672463898339dc6c58ad49
Francisco Vargas <falisson.sv@gmail.com>
2020-04-04 21:01:44 -0300

    Change api url

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const api = axios.create({
     /* baseURL: 'https://ditudo-backend.herokuapp.com', */
-    baseURL: 'http://localhost:3001',
+    baseURL: 'http://35.222.146.62:3001',
 })
 
 export default api;
